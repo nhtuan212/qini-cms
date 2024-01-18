e5423d54404361d1ff28e6a44f662b446b50970d
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-01-18 16:16:43 +0700

    Refactor pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,6 +3,11 @@
 import React, { useState } from "react";
 import clsx from "clsx";
 import Button from "./Button";
+import {
+    ChevronDoubleLeftIcon,
+    ChevronDoubleRightIcon,
+    EllipsisHorizontalIcon,
+} from "@heroicons/react/24/outline";
 
 interface PaginationProps {
     className?: string;
@@ -16,27 +21,29 @@ export default function Pagination({
     total,
 }: PaginationProps) {
     //** States */
-    const [active, setActive] = useState<number>(initialPage);
     const [currentPage, setCurrentPage] = useState<number>(initialPage);
 
     //** Variables */
+    const firstPage = initialPage;
+    const lastPage = total;
+
     const paginationNumbers = [];
-    const range = 5;
-    const limit =
-        currentPage < range
-            ? range
-            : currentPage + 2 > total
-              ? total
-              : currentPage + 2;
+    // const range = 5;
+    // const limit =
+    //     currentPage < range
+    //         ? range
+    //         : currentPage + 2 > total
+    //           ? total
+    //           : currentPage + 2;
 
-    const prev =
-        currentPage < range
-            ? 1
-            : currentPage + 2 > total
-              ? total - range + 1
-              : currentPage - 2;
+    // const prev =
+    //     currentPage < range
+    //         ? 1
+    //         : currentPage + 2 > total
+    //           ? total - range + 1
+    //           : currentPage - 2;
 
-    for (let i: number = prev; i <= limit; i++) {
+    for (let i: number = firstPage; i <= total; i++) {
         if (i < 1) continue;
         if (i > total) break;
         paginationNumbers.push(i);
@@ -44,25 +51,64 @@ export default function Pagination({
 
     //** Functions */
     const handleChangePage = (value: number) => {
-        setActive(value);
         setCurrentPage(value);
     };
+
+    const handlePageLoadLess = () => {
+        setCurrentPage(firstPage);
+    };
+
+    const handlePageLoadMore = () => {
+        setCurrentPage(lastPage);
+    };
+
+    const handleFirstPage = () => {
+        setCurrentPage(firstPage);
+    };
+
+    const handleLastPage = () => {
+        setCurrentPage(lastPage);
+    };
+
+    if (!total || total <= 1) return null;
 
     return (
         <div className={clsx("flex w-full h-full", className)}>
             <nav className="pagination">
+                <Button className="bg-gray-200" onClick={handleFirstPage}>
+                    <ChevronDoubleLeftIcon className="w-4" />
+                </Button>
+
+                <Button
+                    className="items-end bg-transparent p-0"
+                    onClick={handlePageLoadLess}
+                >
+                    <EllipsisHorizontalIcon className="w-4" />
+                </Button>
+
                 {paginationNumbers.map(pageItem => (
                     <Button
                         key={pageItem}
                         className={clsx(
                             "pagination-item",
-                            active === pageItem && "pagination-active",
+                            currentPage === pageItem && "pagination-active",
                         )}
                         onClick={() => handleChangePage(pageItem)}
                     >
                         {pageItem}
                     </Button>
                 ))}
+
+                <Button
+                    className="items-end bg-transparent p-0"
+                    onClick={handlePageLoadMore}
+                >
+                    <EllipsisHorizontalIcon className="w-4" />
+                </Button>
+
+                <Button className="bg-gray-200" onClick={handleLastPage}>
+                    <ChevronDoubleRightIcon className="w-4" />
+                </Button>
             </nav>
         </div>
     );
