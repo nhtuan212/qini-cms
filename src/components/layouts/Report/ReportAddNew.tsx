53b399ee3ac999dd706d1bcdfb777adde31052c1
Hoang Tuan <nhtuan212@gmail.com>
2024-01-21 17:01:21 +0700

    Add react hook form and check validate fields array

--- src/components/layouts/Report/ReportAddNew.tsx
+++ src/components/layouts/Report/ReportAddNew.tsx
@@ -10,14 +10,40 @@ import {
     PlusIcon,
     UserCircleIcon,
 } from "@heroicons/react/24/outline";
+import { useFieldArray, useForm, Controller } from "react-hook-form";
 import { useProfileStore } from "@/stores/useProfileStore";
 import { staffApi, timeSheet } from "@/config/apis";
 import { TEXT } from "@/constants/text";
+
+type FormValues = {
+    staff: {
+        name: string;
+        checkIn: string;
+        checkOut: string;
+    }[];
+};
 
 export default function ReportAddNew() {
     //** Stores */
     const { profile } = useProfileStore();
     const { openModal, isModalOpen } = useModalStore();
+
+    //** React hook form */
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>({
+        defaultValues: {
+            staff: [{ name: "", checkIn: "", checkOut: "" }],
+        },
+        mode: "onBlur",
+    });
+    const { fields, append, remove } = useFieldArray({
+        name: "staff",
+        control,
+    });
+    const onSubmit = (data: FormValues) => console.log(data);
 
     return (
         <Modal
@@ -37,41 +63,123 @@ export default function ReportAddNew() {
                             {TEXT.DATE}: {new Date().toDateString()}
                         </p>
                     </div>
-                    <div className="w-full flex justify-between items-center gap-3">
-                        <Select
-                            className="w-full"
-                            startContent={<UserCircleIcon className="w-6" />}
-                            label={TEXT.STAFF}
+
+                    <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
+                        {fields.map((field, index) => {
+                            return (
+                                <div
+                                    key={field.id}
+                                    className="w-full flex justify-between items-center gap-3"
+                                >
+                                    <Controller
+                                        name={`staff.${index}.name`}
+                                        control={control}
+                                        rules={{ required: true }}
+                                        render={({ field: { onChange } }) => (
+                                            <Select
+                                                className="w-full"
+                                                startContent={
+                                                    <UserCircleIcon className="w-6" />
+                                                }
+                                                label={TEXT.STAFF}
+                                                onChange={onChange}
+                                                errorMessage={
+                                                    errors?.staff?.[index]
+                                                        ?.name &&
+                                                    "Name is required"
+                                                }
+                                            >
+                                                {staffApi.map(item => (
+                                                    <SelectItem
+                                                        key={item.id}
+                                                        value={item.value}
+                                                    >
+                                                        {item.value}
+                                                    </SelectItem>
+                                                ))}
+                                            </Select>
+                                        )}
+                                    />
+                                    <Controller
+                                        name={`staff.${index}.checkIn`}
+                                        control={control}
+                                        rules={{ required: true }}
+                                        render={({ field: { onChange } }) => (
+                                            <Select
+                                                className="w-full"
+                                                startContent={
+                                                    <ClockIcon className="w-6" />
+                                                }
+                                                label={TEXT.CHECK_IN}
+                                                onChange={onChange}
+                                                errorMessage={
+                                                    errors?.staff?.[index]
+                                                        ?.checkIn &&
+                                                    "Check-in is required"
+                                                }
+                                            >
+                                                {timeSheet.map(item => (
+                                                    <SelectItem
+                                                        key={item.id}
+                                                        value={item.value}
+                                                    >
+                                                        {item.value}
+                                                    </SelectItem>
+                                                ))}
+                                            </Select>
+                                        )}
+                                    />
+                                    <Controller
+                                        name={`staff.${index}.checkOut`}
+                                        control={control}
+                                        rules={{ required: true }}
+                                        render={({ field: { onChange } }) => (
+                                            <Select
+                                                className="w-full"
+                                                startContent={
+                                                    <ClockIcon className="w-6" />
+                                                }
+                                                label={TEXT.CHECK_OUT}
+                                                onChange={onChange}
+                                                errorMessage={
+                                                    errors?.staff?.[index]
+                                                        ?.checkOut &&
+                                                    "Check-out is required"
+                                                }
+                                            >
+                                                {timeSheet.map(item => (
+                                                    <SelectItem
+                                                        key={item.id}
+                                                        value={item.value}
+                                                    >
+                                                        {item.value}
+                                                    </SelectItem>
+                                                ))}
+                                            </Select>
+                                        )}
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() => remove(index)}
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            );
+                        })}
+                        <Button className="mt-4" type="submit">
+                            {TEXT.SAVE}
+                        </Button>
+                        <button
+                            type="button"
+                            onClick={() =>
+                                append({ name: "", checkIn: "", checkOut: "" })
+                            }
                         >
-                            {staffApi.map(item => (
-                                <SelectItem key={item.id} value={item.value}>
-                                    {item.value}
-                                </SelectItem>
-                            ))}
-                        </Select>
-                        <Select
-                            className="w-full"
-                            startContent={<ClockIcon className="w-6" />}
-                            label={TEXT.CHECK_IN}
-                        >
-                            {timeSheet.map(item => (
-                                <SelectItem key={item.id} value={item.value}>
-                                    {item.value}
-                                </SelectItem>
-                            ))}
-                        </Select>
-                        <Select
-                            className="w-full"
-                            startContent={<ClockIcon className="w-6" />}
-                            label={TEXT.CHECK_OUT}
-                        >
-                            {timeSheet.map(item => (
-                                <SelectItem key={item.id} value={item.value}>
-                                    {item.value}
-                                </SelectItem>
-                            ))}
-                        </Select>
-                    </div>
+                            append
+                        </button>
+                    </form>
+
                     <div className="w-full flex justify-end">
                         <Button>
                             <PlusIcon className="w-5 mr-2" />
