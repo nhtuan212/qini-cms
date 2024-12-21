9da7f741a237028dd494c24368f57e24e7d51dcb
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2024-12-22 00:15:49 +0700

    Update middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,45 @@
+// import { auth } from "./auth";
+
+// export default auth(req => {
+//     if (
+//         !req.auth &&
+//         req.nextUrl.pathname !== "/login" &&
+//         req.nextUrl.pathname !== "/user/set-password"
+//     ) {
+//         // Redirect to login page with callback URL
+//         const newUrl = new URL("/login", req.nextUrl.origin);
+//         newUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
+
+//         return Response.redirect(newUrl);
+//     }
+//     if (req.auth) {
+//         switch (req.nextUrl.pathname) {
+//             case "/":
+//             case "/login":
+//                 return Response.redirect(new URL("/bao-cao", req.url));
+//             default:
+//                 break;
+//         }
+//     }
+// });
+
+// export const config = {
+//     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
+
 import { auth } from "./auth";
 
 export default auth(req => {
-    if (
-        !req.auth &&
-        req.nextUrl.pathname !== "/login" &&
-        req.nextUrl.pathname !== "/user/set-password"
-    ) {
+    if (!req.auth && req.nextUrl.pathname !== "/login") {
         // Redirect to login page with callback URL
         const newUrl = new URL("/login", req.nextUrl.origin);
         newUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
-
         return Response.redirect(newUrl);
     }
-    if (req.auth) {
-        switch (req.nextUrl.pathname) {
-            case "/":
-            case "/login":
-                return Response.redirect(new URL("/bao-cao", req.url));
-            default:
-                break;
-        }
+    if (req.auth && req.nextUrl.pathname === "/login") {
+        // User is authenticated and trying to access login page
+        // Redirect to home page
+        return Response.redirect(new URL("/", req.url));
     }
 });
 
