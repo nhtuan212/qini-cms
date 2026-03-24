1639e7bfac62b22933009e0a7bb315377da266ac
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2026-03-24 15:51:43 +0900

    Update debounce custom hook and apply it into button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,25 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { Button as ButtonNextUI, ButtonProps } from "@heroui/react";
+import { useDebounce } from "@/hooks/useDebounce";
 
 const Button = React.forwardRef(({ ...props }: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+    //** States */
+    const isSubmitting = useRef(false);
+
+    //** Functions */
+    const handleClick = useDebounce(async () => {
+        if (typeof props.onPress !== "function") return;
+
+        isSubmitting.current = true;
+
+        await Promise.resolve(props.onPress({} as any)).finally(() => {
+            isSubmitting.current = false;
+        });
+    });
+
+    //** Render */
     return (
         <ButtonNextUI
             ref={ref}
@@ -11,6 +27,7 @@ const Button = React.forwardRef(({ ...props }: ButtonProps, ref: React.Ref<HTMLB
             color={props.color || "primary"}
             radius={props.radius || "sm"}
             {...props}
+            onPress={handleClick}
         >
             {props.children}
         </ButtonNextUI>
