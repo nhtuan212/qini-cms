dc62b99593ac19a7927c51b442591c03a90f45ce
Nguyen Hoang Tuan <nguyen_hoangtuan@ca-adv.co.jp>
2026-04-09 17:56:38 +0900

    Update english comment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
     experimental: {
         staleTimes: {
-            dynamic: 30, // cache dynamic route 30 giây (mặc định 0)
-            static: 180, // cache static route 3 phút
+            dynamic: 30, // cache dynamic route 30 seconds (default 0)
+            static: 180, // cache static route 3 mins
         },
     },
 };
